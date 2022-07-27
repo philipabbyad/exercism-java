2cb1827b33af7c3093a7cd3658ed9f06ce29d165
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-07-27 15:57:16 -0600

    Replaced if statement with ternary to simplify method.

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,12 +1,5 @@
-
 public class Twofer {
     public String twofer(String name) {
-        if (name == null) {
-            return String.format("One for you, one for me.");
-        } else {
-            return String.format("One for %s, one for me.", name);
-        }
-
+        return String.format("One for %s, one for me.", (name == null) ? "you" : name);
     }
-
 }
