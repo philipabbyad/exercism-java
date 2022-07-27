61be4573542fbf79738e9bc33183249d6a0ed01f
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-07-27 15:50:50 -0600

    Changed format to String.format to make import statement unnessecary.

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,11 +1,10 @@
-import static java.lang.String.format;
 
 public class Twofer {
     public String twofer(String name) {
         if (name == null) {
-            return format("One for you, one for me.");
+            return String.format("One for you, one for me.");
         } else {
-            return format("One for %s, one for me.", name);
+            return String.format("One for %s, one for me.", name);
         }
 
     }
