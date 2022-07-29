cc282dd3473f53c36426beb45014f691ec6ed616
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-07-29 16:28:18 -0600

    Created constants for prep and cook time.

--- lasagna/src/main/java/Lasagna.java
+++ lasagna/src/main/java/Lasagna.java
@@ -1,6 +1,10 @@
 public class Lasagna {
+
+    private static final int TOTAL_OVEN_TIME_MINUTES = 40;
+    private static final int LAYER_PREP_TIME_MINUTES = 2;
+
     public int expectedMinutesInOven() {
-        return 40;
+        return TOTAL_OVEN_TIME_MINUTES;
     }
 
     public int remainingMinutesInOven(int minutesInOvenSoFar) {
@@ -8,7 +12,7 @@ public class Lasagna {
     }
 
     public int preparationTimeInMinutes(int numberOfLayers) {
-        return (numberOfLayers * 2);
+        return (numberOfLayers * LAYER_PREP_TIME_MINUTES);
     }
 
     public int totalTimeInMinutes(int numberOfLayers, int minutesInOvenSoFar) {
