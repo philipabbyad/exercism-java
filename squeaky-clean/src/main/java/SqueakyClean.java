2d03bb3962aa993187aa1b1ac9ec5d3d2fc0d071
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-07-30 14:17:17 -0600

    Removed control characters from argument in clean method using alternate regex statement using predifined list of unicode control characters.

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -15,7 +15,8 @@ class SqueakyClean {
 //            cleanIdentifier = identifier.replaceAll("I", "CTRL");
 //        }
 
-        cleanIdentifier = identifier.replaceAll("[\\p{Cntrl}]", "CTRL");
+//        cleanIdentifier = identifier.replaceAll("[\\p{Cntrl}]", "CTRL");
+        cleanIdentifier = identifier.replaceAll("[\u0000-\u001F\u007F-\u009F]", "CTRL");
 
 
     return cleanIdentifier;
