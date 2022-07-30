1e2bcec2876585319ee21e375e19373b495240c1
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-07-30 13:53:57 -0600

    Removed control characters from argument in clean method using regex.

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,11 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 class SqueakyClean {
     private static String cleanIdentifier;
 
     static String clean(String identifier) {
         cleanIdentifier = identifier;
+
         if (identifier.contains(" ")) {
             cleanIdentifier =  identifier.replace(' ', '_');
         }
+
+//        while (identifier.contains("")) {
+//            cleanIdentifier = identifier.replaceAll("I", "CTRL");
+//        }
+
+        cleanIdentifier = identifier.replaceAll("[\\p{Cntrl}]", "CTRL");
+
+
     return cleanIdentifier;
     }
 }
