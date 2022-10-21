39ae8415b9a5c5a5da038c83a430d3c49281395f
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-10-21 14:13:06 -0600

    Completed Squeaky Clean exercise - all tests pass

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,25 +1,15 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 class SqueakyClean {
     private static String cleanIdentifier;
 
     static String clean(String identifier) {
         cleanIdentifier = identifier;
 
-        //Replace blank space with underscores:
-
+        // 1) Replace blank space with underscores:
         if (identifier.contains(" ")) {
             cleanIdentifier =  cleanIdentifier.replace(' ', '_');
         }
 
-        //Remove Control Characters:
-
-//        cleanIdentifier = cleanIdentifier.replaceAll("[\\p{Cntrl}]", "CTRL");
-//          OR
-//        cleanIdentifier = cleanIdentifier.replaceAll("[\u0000-\u001F\u007F]", "CTRL");
-//          OR
-
+        // 2) Remove Control Characters:
         for (int i = 0; i < cleanIdentifier.length(); i++) {
             int codePoint = cleanIdentifier.codePointAt(i);
             if ((codePoint >= 0 && codePoint <= 31) || codePoint == 127) {
@@ -27,8 +17,23 @@ class SqueakyClean {
             }
         }
 
+        // 3) Convert kebab-case to camelCase
+        while (cleanIdentifier.indexOf('-') != -1) {
+            int index = cleanIdentifier.indexOf('-');
+            cleanIdentifier = cleanIdentifier.substring(0, index) + cleanIdentifier.substring(index + 1, index + 2).toUpperCase() + cleanIdentifier.substring(index + 2);
+        }
 
 
+        // 4+5) Omit characters that are not letters and Greek letters in the range 'α' to 'ω'
+        char[] cleanIdentifierCharArray = cleanIdentifier.toCharArray();
+        StringBuilder cleanIdentifierStringBuilder = new StringBuilder();
+
+        for (char ch : cleanIdentifierCharArray) {
+            if ((Character.isLetter(ch) || ch == '_') && (ch < 'α' || ch > 'ω')) {
+                cleanIdentifierStringBuilder.append(ch);
+            }
+        }
+        cleanIdentifier = cleanIdentifierStringBuilder.toString();
 
     return cleanIdentifier;
     }
