1fcbf94a6a34f6c7e841179ec330d68102698ab3
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-10-21 17:52:43 -0600

    Refactoring code

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,21 +1,24 @@
 class SqueakyClean {
     private static String cleanIdentifier;
 
+
     static String clean(String identifier) {
         cleanIdentifier = identifier;
 
         // 1) Replace blank space with underscores:
-        if (identifier.contains(" ")) {
-            cleanIdentifier =  cleanIdentifier.replace(' ', '_');
-        }
+        // if (identifier.contains(" ")) {
+        //     cleanIdentifier =  cleanIdentifier.replace(' ', '_');
+        // }
+
+        cleanIdentifier = cleanIdentifier.replaceAll(" ", "_");
 
         // 2) Remove Control Characters:
-        for (int i = 0; i < cleanIdentifier.length(); i++) {
-            int codePoint = cleanIdentifier.codePointAt(i);
-            if ((codePoint >= 0 && codePoint <= 31) || codePoint == 127) {
-                cleanIdentifier = cleanIdentifier.substring(0, i) + "CTRL" + cleanIdentifier.substring(i +1);
-            }
-        }
+        // for (int i = 0; i < cleanIdentifier.length(); i++) {
+        //     int codePoint = cleanIdentifier.codePointAt(i);
+        //     if ((codePoint >= 0 && codePoint <= 31) || codePoint == 127) {
+        //         cleanIdentifier = cleanIdentifier.substring(0, i) + "CTRL" + cleanIdentifier.substring(i +1);
+        //     }
+        // }
 
         // 3) Convert kebab-case to camelCase
         while (cleanIdentifier.indexOf('-') != -1) {
@@ -24,13 +27,16 @@ class SqueakyClean {
         }
 
 
-        // 4+5) Omit characters that are not letters and Greek letters in the range 'α' to 'ω'
+        // 2+4+5) Replace Control Characters with 'CTRL'; Omit characters that are not letters and Greek letters in the range 'α' to 'ω'
         char[] cleanIdentifierCharArray = cleanIdentifier.toCharArray();
         StringBuilder cleanIdentifierStringBuilder = new StringBuilder();
 
         for (char ch : cleanIdentifierCharArray) {
             if ((Character.isLetter(ch) || ch == '_') && (ch < 'α' || ch > 'ω')) {
                 cleanIdentifierStringBuilder.append(ch);
+            }
+            if ((ch >= 0 && ch <= 31) || ch == 127) {
+                cleanIdentifierStringBuilder.append("CTRL");
             }
         }
         cleanIdentifier = cleanIdentifierStringBuilder.toString();
