bfaa4832b1aa51d636f365c8ef6b495d4e616dbb
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-10-21 18:42:02 -0600

    Separated clean methods into separate methods for each purpose.

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,46 +1,67 @@
 class SqueakyClean {
     private static String cleanIdentifier;
 
+    static String replaceSpaces(String identifier) {
+        return identifier.replaceAll(" ", "_");
+    }
+
+    static String replaceControlCharacters(String identifier) {
+        char[] identifierCharArray = identifier.toCharArray();
+        StringBuilder identifierStringBuilder = new StringBuilder();
+
+        for (char ch : identifierCharArray) {
+            if ((ch >= 0 && ch <= 31) || ch == 127) {
+                identifierStringBuilder.append("CTRL");
+            } else {
+                identifierStringBuilder.append(ch);
+            }
+        }
+        return identifierStringBuilder.toString();
+    }
+
+    static String convertKebabCaseToCamelCase(String identifier) {
+        String identifierCamelCase = identifier;
+        while (identifierCamelCase.indexOf('-') != -1) {
+            int index = identifierCamelCase.indexOf('-');
+            identifierCamelCase = identifierCamelCase.substring(0, index) + identifierCamelCase.substring(index + 1, index + 2).toUpperCase() + identifierCamelCase.substring(index + 2);
+        }
+        return identifierCamelCase;
+    }
+
+    static String removeNonLetters(String identifier) {
+        char[] identifierCharArray = identifier.toCharArray();
+        StringBuilder identifierStringBuilder = new StringBuilder();
+
+        for (char ch : identifierCharArray) {
+            if (Character.isLetter(ch) || ch == '_') {
+                identifierStringBuilder.append(ch);
+            }
+        }
+        return identifierStringBuilder.toString();
+    }
+
+    static String removeGreekLowercaseLetters(String identifier) {
+        char[] identifierCharArray = identifier.toCharArray();
+        StringBuilder identifierStringBuilder = new StringBuilder();
+
+        for (char ch : identifierCharArray) {
+            if (ch < 'α' || ch > 'ω') {
+                identifierStringBuilder.append(ch);
+            }
+        }
+        return identifierStringBuilder.toString();
+    }
+
 
     static String clean(String identifier) {
         cleanIdentifier = identifier;
 
-        // 1) Replace blank space with underscores:
-        // if (identifier.contains(" ")) {
-        //     cleanIdentifier =  cleanIdentifier.replace(' ', '_');
-        // }
+        cleanIdentifier = SqueakyClean.replaceSpaces(cleanIdentifier);
+        cleanIdentifier = SqueakyClean.replaceControlCharacters(cleanIdentifier);
+        cleanIdentifier = SqueakyClean.convertKebabCaseToCamelCase(cleanIdentifier);
+        cleanIdentifier = SqueakyClean.removeNonLetters(cleanIdentifier);
+        cleanIdentifier = SqueakyClean.removeGreekLowercaseLetters(cleanIdentifier);
 
-        cleanIdentifier = cleanIdentifier.replaceAll(" ", "_");
-
-        // 2) Remove Control Characters:
-        // for (int i = 0; i < cleanIdentifier.length(); i++) {
-        //     int codePoint = cleanIdentifier.codePointAt(i);
-        //     if ((codePoint >= 0 && codePoint <= 31) || codePoint == 127) {
-        //         cleanIdentifier = cleanIdentifier.substring(0, i) + "CTRL" + cleanIdentifier.substring(i +1);
-        //     }
-        // }
-
-        // 3) Convert kebab-case to camelCase
-        while (cleanIdentifier.indexOf('-') != -1) {
-            int index = cleanIdentifier.indexOf('-');
-            cleanIdentifier = cleanIdentifier.substring(0, index) + cleanIdentifier.substring(index + 1, index + 2).toUpperCase() + cleanIdentifier.substring(index + 2);
-        }
-
-
-        // 2+4+5) Replace Control Characters with 'CTRL'; Omit characters that are not letters and Greek letters in the range 'α' to 'ω'
-        char[] cleanIdentifierCharArray = cleanIdentifier.toCharArray();
-        StringBuilder cleanIdentifierStringBuilder = new StringBuilder();
-
-        for (char ch : cleanIdentifierCharArray) {
-            if ((Character.isLetter(ch) || ch == '_') && (ch < 'α' || ch > 'ω')) {
-                cleanIdentifierStringBuilder.append(ch);
-            }
-            if ((ch >= 0 && ch <= 31) || ch == 127) {
-                cleanIdentifierStringBuilder.append("CTRL");
-            }
-        }
-        cleanIdentifier = cleanIdentifierStringBuilder.toString();
-
-    return cleanIdentifier;
+        return cleanIdentifier;
     }
 }
