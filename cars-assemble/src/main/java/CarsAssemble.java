0f36c1a6ffb5c0c02d8be6726c35b776113033d9
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-11-18 10:54:13 -0700

    Complated Task 1 - productionRatePerHour method, all tests for method pass.

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,7 +1,16 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRateperHour() method");
+        double productionRate = speed * 221;
+
+        if (speed >= 5 && speed <= 8) {
+            productionRate *= .9;
+        } else if (speed == 9) {
+            productionRate *= .8;
+        } else if (speed >= 10) {
+            productionRate *= .77;
+        }
+        return productionRate;
     }
 
     public int workingItemsPerMinute(int speed) {
