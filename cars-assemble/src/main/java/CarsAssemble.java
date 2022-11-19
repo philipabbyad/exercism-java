983eb24117f28faae3b8f01c48cacec827917b11
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-11-18 17:18:07 -0700

    Refactored productionRatePerHour method to handle all values for speed

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -3,11 +3,15 @@ public class CarsAssemble {
     public double productionRatePerHour(int speed) {
         double productionRate = speed * 221;
 
-        if (speed >= 5 && speed <= 8) {
+        if (speed <= 0) {
+            productionRate *= 0;
+        } else if (speed >= 1 && speed <= 4) {
+            productionRate *= 1;
+        } else if (speed >= 5 && speed <= 8) {
             productionRate *= .9;
         } else if (speed == 9) {
             productionRate *= .8;
-        } else if (speed >= 10) {
+        } else {
             productionRate *= .77;
         }
         return productionRate;
