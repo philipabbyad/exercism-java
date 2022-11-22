f91fecab01d178eb3eb4d801703b01ae6443126d
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-11-21 18:17:33 -0700

    Completed Task 2: addLanguage method.

--- karls-languages/src/main/java/LanguageList.java
+++ karls-languages/src/main/java/LanguageList.java
@@ -10,7 +10,7 @@ public class LanguageList {
     }
 
     public void addLanguage(String language) {
-        throw new UnsupportedOperationException("Please implement the addLanguage() method");
+        languages.add(language);
     }
 
     public void removeLanguage(String language) {
