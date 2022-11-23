ef919de5603618a96d06163cfb0228fb330d6f38
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-11-22 17:59:25 -0700

    Created fields and Constructor for Scrabble class.

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -1,7 +1,22 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 class Scrabble {
 
-    Scrabble(String word) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private String word = "";
+
+    HashMap<List<Character>, Integer> scrabbleLetterValues = new HashMap<>();
+
+    public Scrabble(String word) {
+        this.word = word;
+        scrabbleLetterValues.put(Arrays.asList('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'), 1);
+        scrabbleLetterValues.put(Arrays.asList('D', 'G'), 2);
+        scrabbleLetterValues.put(Arrays.asList('B', 'C', 'M', 'P'), 3);
+        scrabbleLetterValues.put(Arrays.asList('F', 'H', 'V', 'W', 'Y'), 4);
+        scrabbleLetterValues.put(Arrays.asList('K'), 5);
+        scrabbleLetterValues.put(Arrays.asList('J', 'X'), 8);
+        scrabbleLetterValues.put(Arrays.asList('Q', 'Z'), 10);
     }
 
     int getScore() {
