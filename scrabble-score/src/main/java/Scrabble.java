4732bd187b6abd65be013c625fce789e882872a1
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-11-28 16:00:08 -0700

    made hashmap for scores constant.

--- scrabble-score/src/main/java/Scrabble.java
+++ scrabble-score/src/main/java/Scrabble.java
@@ -6,24 +6,24 @@ class Scrabble {
 
     private String word = "";
 
-    HashMap<List<Character>, Integer> scrabbleLetterValues = new HashMap<>();
+    static final public HashMap<List<Character>, Integer> SCRABBLE_LETTER_VALUES = new HashMap<>();
 
     public Scrabble(String word) {
         this.word = word;
-        scrabbleLetterValues.put(Arrays.asList('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'), 1);
-        scrabbleLetterValues.put(Arrays.asList('D', 'G'), 2);
-        scrabbleLetterValues.put(Arrays.asList('B', 'C', 'M', 'P'), 3);
-        scrabbleLetterValues.put(Arrays.asList('F', 'H', 'V', 'W', 'Y'), 4);
-        scrabbleLetterValues.put(Arrays.asList('K'), 5);
-        scrabbleLetterValues.put(Arrays.asList('J', 'X'), 8);
-        scrabbleLetterValues.put(Arrays.asList('Q', 'Z'), 10);
+        SCRABBLE_LETTER_VALUES.put(Arrays.asList('A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'), 1);
+        SCRABBLE_LETTER_VALUES.put(Arrays.asList('D', 'G'), 2);
+        SCRABBLE_LETTER_VALUES.put(Arrays.asList('B', 'C', 'M', 'P'), 3);
+        SCRABBLE_LETTER_VALUES.put(Arrays.asList('F', 'H', 'V', 'W', 'Y'), 4);
+        SCRABBLE_LETTER_VALUES.put(Arrays.asList('K'), 5);
+        SCRABBLE_LETTER_VALUES.put(Arrays.asList('J', 'X'), 8);
+        SCRABBLE_LETTER_VALUES.put(Arrays.asList('Q', 'Z'), 10);
     }
 
     int getScore() {
         int wordScore = 0;
         char[] wordCharacterArray = this.word.toUpperCase().toCharArray();
         for (char wordCharacter : wordCharacterArray) {
-            for (HashMap.Entry<List<Character>, Integer> scrabbleLetterValue : scrabbleLetterValues.entrySet()) {
+            for (HashMap.Entry<List<Character>, Integer> scrabbleLetterValue : SCRABBLE_LETTER_VALUES.entrySet()) {
                 if (scrabbleLetterValue.getKey().contains(wordCharacter)) {
                     wordScore += scrabbleLetterValue.getValue();
                 }
