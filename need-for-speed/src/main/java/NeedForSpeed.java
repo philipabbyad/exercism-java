01677301a259ec9bbdc4f92cc7bd7a6540c1f7f4
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-12-02 15:10:42 -0700

    Created constructor for RaceTrack class

--- need-for-speed/src/main/java/NeedForSpeed.java
+++ need-for-speed/src/main/java/NeedForSpeed.java
@@ -26,7 +26,12 @@ class NeedForSpeed {
 }
 
 class RaceTrack {
-    // TODO: define the constructor for the 'RaceTrack' class
+    
+    private int distance;
+
+    public RaceTrack(int distance) {
+        this.distance = distance;
+    }
 
     public boolean tryFinishTrack(NeedForSpeed car) {
         throw new UnsupportedOperationException("Please implement the RaceTrack.tryFinishTrack() method");
