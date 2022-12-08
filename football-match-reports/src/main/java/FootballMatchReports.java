ac05b1133cfb7bdcb812313faaa84eeb16542d81
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2022-12-07 17:07:38 -0700

    Began onField()

--- football-match-reports/src/main/java/FootballMatchReports.java
+++ football-match-reports/src/main/java/FootballMatchReports.java
@@ -1,5 +1,15 @@
 public class FootballMatchReports {    
+    
     public static String onField(int shirtNum) {
-        throw new UnsupportedOperationException("Please implement the (static) FootballMatchReports.onField() method");
+        String playerPosition = "";
+        switch (playerPosition) {
+            case 1:
+                "goalie";
+                break;
+
+        }
+        
+        
+        return playerPosition;
     }
 }
