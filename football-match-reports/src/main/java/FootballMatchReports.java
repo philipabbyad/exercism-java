075f1d9b0298752280579af4c9c2caa38b9aa411
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-01-09 15:10:29 -0700

    Completed Part 1: All tests passing

--- football-match-reports/src/main/java/FootballMatchReports.java
+++ football-match-reports/src/main/java/FootballMatchReports.java
@@ -2,14 +2,39 @@ public class FootballMatchReports {
     
     public static String onField(int shirtNum) {
         String playerPosition = "";
-        switch (playerPosition) {
-            case 1:
-                "goalie";
-                break;
 
+        switch (shirtNum) {
+            case 1:
+                playerPosition = "goalie";
+                break;
+            case 2:
+                playerPosition = "left back";
+                break;
+            case 3:
+            case 4:
+                playerPosition = "center back";
+                break;
+            case 5:
+                playerPosition = "right back";
+                break;
+            case 6:
+            case 7:
+            case 8:
+                playerPosition = "midfielder";
+                break;
+            case 9:
+                playerPosition = "left wing";
+                break;
+            case 10:
+                playerPosition = "striker";
+                break;
+            case 11:
+                playerPosition = "right wing";
+                break;
+            default: break;
         }
-        
-        
+
+           
         return playerPosition;
     }
 }
