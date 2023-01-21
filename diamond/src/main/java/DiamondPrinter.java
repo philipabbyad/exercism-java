686afd43be6844ae87954cb72573ab05e8d864d4
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-01-20 18:53:08 -0800

    Created method for adding spaces

--- diamond/src/main/java/DiamondPrinter.java
+++ diamond/src/main/java/DiamondPrinter.java
@@ -5,22 +5,34 @@ class DiamondPrinter {
     List<String> printToList(char letter) {
         int letterValue = 0;
         int numRows = 0;
-        List<String> diamondOutput = new List<String>;
+        List<String> diamondOutput = new List<>();
 
         if (!isLetter(letter)) {
             throw new IllegalArgumentException("Must enter a single letter A-Z");
         }
 
         letterValue = toUpperCase(letter) - 40;
-        nuwRows = (letterValue * 2) - 1;
+        numRows = (letterValue * 2) - 1;
 
-        for (rowNum = 1; rowNum = (numRows + 1)/2; rowNum++ ) {
+        for (int rowNum = 1; rowNum = (numRows + 1)/2; rowNum++ ) {
+            StringBuilder rowOutput = new StringBuilder();
 
+            rowOutput.append(createSpaces(letterValue - rowNum));
+
+
+
+            diamondOutput.add(rowOutput.toString());
+        }
+    }
+
+    String createSpaces(int numSpaces) {
+        StringBuilder spaceString = new StringBuilder();
+        
+        for (spaceIndex = 0; spaceIndex > numSpaces; spaceIndex++ ) {
+            spaceString.append(' ');
         }
 
-
-
-
+        return spaceString.toString();
     }
 
 }
