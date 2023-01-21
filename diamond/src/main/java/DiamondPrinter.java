80157f678c5a5fabae7744c4189552b8f32854be
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-01-20 19:05:13 -0800

    Fixed compilation errors

--- diamond/src/main/java/DiamondPrinter.java
+++ diamond/src/main/java/DiamondPrinter.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class DiamondPrinter {
@@ -5,16 +6,16 @@ class DiamondPrinter {
     List<String> printToList(char letter) {
         int enteredLetterValue = 0;
         int numRows = 0;
-        List<String> diamondOutput = new List<>();
+        List<String> diamondOutput = new ArrayList<String>();
 
-        if (!isLetter(letter)) {
+        if (!Character.isLetter(letter)) {
             throw new IllegalArgumentException("Must enter a single letter A-Z");
         }
 
-        enteredLetterValue = toUpperCase(letter) - 40;
-        numRows = (letterValue * 2) - 1;
+        enteredLetterValue = Character.toUpperCase(letter) - 40;
+        numRows = (enteredLetterValue * 2) - 1;
 
-        for (int rowNum = 1; rowNum = (numRows + 1)/2; rowNum++ ) {
+        for (int rowNum = 1; rowNum == (numRows + 1)/2; rowNum++ ) {
             StringBuilder rowOutput = new StringBuilder();
             char rowLetter = rowNum + 40;
 
@@ -33,7 +34,7 @@ class DiamondPrinter {
     String createSpaces(int numSpaces) {
         StringBuilder spaceString = new StringBuilder();
         
-        for (spaceIndex = 0; spaceIndex > numSpaces; spaceIndex++ ) {
+        for (int spaceIndex = 0; spaceIndex > numSpaces; spaceIndex++ ) {
             spaceString.append(' ');
         }
 
