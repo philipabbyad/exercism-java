6986f6936c2532f85db009f736d5a735c7d88ce9
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-01-20 18:31:49 -0800

    Started first for-loop for first half of diamon output

--- diamond/src/main/java/DiamondPrinter.java
+++ diamond/src/main/java/DiamondPrinter.java
@@ -4,17 +4,23 @@ class DiamondPrinter {
 
     List<String> printToList(char letter) {
         int letterValue = 0;
-        int rows = 0;
+        int numRows = 0;
+        List<String> diamondOutput = new List<String>;
 
         if (!isLetter(letter)) {
-            
+            throw new IllegalArgumentException("Must enter a single letter A-Z");
+        }
+
+        letterValue = toUpperCase(letter) - 40;
+        nuwRows = (letterValue * 2) - 1;
+
+        for (rowNum = 1; rowNum = (numRows + 1)/2; rowNum++ ) {
+
         }
 
 
 
 
-
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 
 }
