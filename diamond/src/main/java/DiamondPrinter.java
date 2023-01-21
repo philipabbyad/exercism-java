974eb568b7768da123274fcadd731235f9dd6151
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-01-20 18:26:04 -0800

    Started diamond exercise

---
+++ diamond/src/main/java/DiamondPrinter.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+class DiamondPrinter {
+
+    List<String> printToList(char letter) {
+        int letterValue = 0;
+        int rows = 0;
+
+        if (!isLetter(letter)) {
+            
+        }
+
+
+
+
+
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
