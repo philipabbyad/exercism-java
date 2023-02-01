7eac8362668e7b45eea1b2c3e1aef087aceb8371
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2023-02-01 14:59:59 -0700

    Started while loop in calculateBasketCost()

--- book-store/src/main/java/BookStore.java
+++ book-store/src/main/java/BookStore.java
@@ -8,6 +8,31 @@ public class BookStore {
 
     public int calculateBasketCost(List<Integer> bookList) {
         int cost = 0;
+        List<Integer> booksRemainingToCheckout = bookList;
+
+            while (booksRemainingToCheckout.size() > 0) {
+                int uniqueBookCount = 0;
+                if (booksRemainingToCheckout.contains(1)) {
+                    booksRemainingToCheckout.remove(1);
+                    uniqueBookCount++;
+                }
+                if (booksRemainingToCheckout.contains(2)) {
+                    booksRemainingToCheckout.remove(2);
+                    uniqueBookCount++;
+                }
+                if (booksRemainingToCheckout.contains(3)) {
+                    booksRemainingToCheckout.remove(3);
+                    uniqueBookCount++;
+                }
+                if (booksRemainingToCheckout.contains(4)) {
+                    booksRemainingToCheckout.remove(4);
+                    uniqueBookCount++;
+                }
+                if (booksRemainingToCheckout.contains(5)) {
+                    booksRemainingToCheckout.remove(5);
+                    uniqueBookCount++;
+                }
+            }
 
         return cost;
     }
